e418b056e5857faa3caa7269525c2f1cf85659f9
Md Zihadur Rahman Zihad <zihadurrahmman@gmail.com>
2026-04-03 21:41:40 +0600

    Assignment 20 done with View Component.

---
+++ aspnet-core-10-net-10-true-ultimate-guide/Assignment20/ViewComponents/WeatherViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Assignment20.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment20.ViewComponents
+{
+    public class WeatherViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather weather)
+        {
+            return View(weather);
+        }
+    }
+}
